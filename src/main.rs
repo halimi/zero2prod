fc3a8ce18c0e8d7c2e472634956a7ce76ae6167e
Imre Halasz <halimi@freemail.hu>
2023-05-14 23:43:36 +0200

    add subscriptions endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
-use zero2prod::run;
 use std::net::TcpListener;
+use zero2prod::run;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-        let listener = TcpListener::bind("127.0.0.1:8000")
-            .expect("Failed to bind address");
+    let listener = TcpListener::bind("127.0.0.1:8000").expect("Failed to bind address");
 
-        run(listener)?.await
+    run(listener)?.await
 }
